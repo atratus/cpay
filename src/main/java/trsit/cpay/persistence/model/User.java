10dfe387413ef4286ca37099b6b609fda98763f7
Dmitry J. Paramzin <dmitry.paramzin@trs-it.ru>
2014-10-31 14:52:41 +0300

    Persistence layer development

--- src/main/java/trsit/cpay/persistence/model/User.java
+++ src/main/java/trsit/cpay/persistence/model/User.java
@@ -3,11 +3,18 @@
  */
 package trsit.cpay.persistence.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * @author black
  *
  */
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private String name;
 
     public String getName() {
