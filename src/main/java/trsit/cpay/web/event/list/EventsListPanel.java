aefcfc87b84342e0b4b2791302421a79a5a15a4a
Dmitry J. Paramzin <dmitry.paramzin@trs-it.ru>
2014-12-02 13:38:06 +0300

    Adding tabs

---
+++ src/main/java/trsit/cpay/web/event/list/EventsListPanel.java
@@ -0,0 +1,155 @@
+/**
+ *
+ */
+package trsit.cpay.web.event.list;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.event.IEvent;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import trsit.cpay.persistence.dao.EventsDAO;
+import trsit.cpay.web.edit.EditEvent;
+import trsit.cpay.web.main.EventActionsPanel;
+import trsit.cpay.web.main.EventItem;
+import trsit.cpay.web.main.EventsProvider;
+
+/**
+ * @author black
+ */
+public class EventsListPanel extends Panel {
+
+    private static final int ROWS_PER_PAGE = 15;
+    private static final String EVENT_TITLE_CLASS = "title-cell";
+    private static final long serialVersionUID = 1L;
+
+    @SpringBean
+    private EventsDAO eventsDAO;
+
+    private DefaultDataTable<EventItem, String> eventsTable;
+
+    private abstract static class CustomStyledColumn<T, S> extends AbstractColumn<T, S> {
+
+        public CustomStyledColumn(IModel<String> displayModel) {
+            super(displayModel);
+        }
+
+    }
+
+    /**
+     * Constructor.
+     */
+    public EventsListPanel(String id) {
+        super(id);
+
+        add(createEventsTable("events"));
+
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+        response.render(JavaScriptHeaderItem.forScript("init()", "init"));
+
+    }
+
+    @Override
+    public void onEvent(IEvent<?> event) {
+        if(event.getPayload() instanceof AjaxRequestTarget) {
+            ((AjaxRequestTarget)event.getPayload()).appendJavaScript("init();");
+        }
+    }
+
+    private Component createEventsTable(String tableComponentName) {
+        List<IColumn<EventItem, String>> columns = new ArrayList<IColumn<EventItem, String>>();
+
+        columns.add(new CustomStyledColumn<EventItem, String>(new Model<String>("Title")) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void populateItem(
+                    Item<ICellPopulator<EventItem>> cellItem,
+                    String componentId,
+                    IModel<EventItem> rowModel) {
+                EventItem data = rowModel.getObject();
+                cellItem.add(new Label(componentId, data.getTitle()));
+            }
+
+            @Override
+            public String getCssClass() {
+                return EVENT_TITLE_CLASS;
+            }
+
+        });
+        columns.add(new CustomStyledColumn<EventItem, String>(new Model<String>("Date")) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void populateItem(
+                    Item<ICellPopulator<EventItem>> cellItem,
+                    String componentId,
+                    IModel<EventItem> rowModel) {
+                EventItem data = rowModel.getObject();
+                cellItem.add(new Label(componentId, new SimpleDateFormat("dd-MM-yyyy hh:mm").format(data
+                        .getCreationTimestamp())));
+
+            }
+
+        });
+        columns.add(new CustomStyledColumn<EventItem, String>(new Model<String>("")) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void populateItem(
+                    final Item<ICellPopulator<EventItem>> cellItem,
+                    String componentId,
+                    final IModel<EventItem> rowModel) {
+                cellItem.add(new EventActionsPanel(componentId) {
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    protected void onRemoveTriggered(AjaxRequestTarget target) {
+                        removeEvent(rowModel.getObject().getId());
+                        target.add(eventsTable);
+                        target.appendJavaScript("init();");
+                    }
+
+                    @Override
+                    protected void onEditTriggered(AjaxRequestTarget target) {
+                        setResponsePage(EditEvent.class,
+                                new PageParameters().add(EditEvent.EVENT_ID, rowModel.getObject().getId()));
+
+                    }
+                });
+
+            }
+        });
+
+        eventsTable =
+                new DefaultDataTable<EventItem, String>(tableComponentName, columns, new EventsProvider(
+                        eventsDAO.getEvents()), ROWS_PER_PAGE);
+        eventsTable.setOutputMarkupId(true);
+        return eventsTable;
+    }
+
+    private void removeEvent(Long id) {
+        eventsDAO.removeEvent(id);
+    }
+
+}
--- src/main/java/trsit/cpay/web/main/MainPage.java
+++ src/main/java/trsit/cpay/web/main/MainPage.java
@@ -1,29 +1,21 @@
 /**
- * 
+ *
  */
 package trsit.cpay.web.main;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.wicket.Component;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.extensions.ajax.markup.html.tabs.AjaxTabbedPanel;
+import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
+import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.model.IModel;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
-import trsit.cpay.persistence.dao.EventsDAO;
-import trsit.cpay.persistence.dao.UserDAO;
 import trsit.cpay.web.edit.EditEvent;
+import trsit.cpay.web.event.list.EventsListPanel;
 import trsit.cpay.web.page.Layout;
 
 /**
@@ -32,107 +24,35 @@ import trsit.cpay.web.page.Layout;
 public class MainPage extends Layout {
     private static final long serialVersionUID = 1L;
 
-    private static final int ROWS_PER_PAGE = 15;
-
-    protected static final String EVENT_TITLE_CLASS = "title-cell";
-    
-    @SpringBean
-    private UserDAO userDAO;
-    
-    @SpringBean
-    private EventsDAO eventsDAO;
-
-    private DefaultDataTable<EventItem, String> eventsTable;
-    
     public MainPage() {
+        add(createTabbedPanel("tabs"));
 
-        add(createEventsTable("events"));
-        
         add(new Link<Void>("add") {
 
             private static final long serialVersionUID = 1L;
 
             @Override
             public void onClick() {
-                /*    setResponsePage(EditEvent.class,
-                new PageParameters().add(EditEvent.EVENT_ID, newEventId));*/
                 setResponsePage(EditEvent.class);
-                
+
             }
         });
 
     }
 
-    private Component createEventsTable(String tableComponentName) {
-        List<IColumn<EventItem, String>> columns = new ArrayList<IColumn<EventItem, String>>();
-
-        columns.add(new AbstractColumn<EventItem, String>(new Model<String>("Title"))
+    private Component createTabbedPanel(String tabbedPanelId) {
+        List<ITab> tabs = new ArrayList<ITab>();
+        tabs.add(new AbstractTab(new Model<String>("first tab"))
         {
             private static final long serialVersionUID = 1L;
 
             @Override
-            public void populateItem(Item<ICellPopulator<EventItem>> cellItem,
-                    String componentId, IModel<EventItem> rowModel) {
-                EventItem data = rowModel.getObject();
-                cellItem.add(new Label(componentId, data.getTitle()));
-            }
-
-            @Override
-            public String getCssClass() {
-                return EVENT_TITLE_CLASS;
-            }
-
-        });
-        columns.add(new AbstractColumn<EventItem, String>(new Model<String>("Date"))
-        {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public void populateItem(Item<ICellPopulator<EventItem>> cellItem,
-                    String componentId, IModel<EventItem> rowModel) {
-                EventItem data = rowModel.getObject();
-                cellItem.add(new Label(componentId, new SimpleDateFormat("dd-MM-yyyy hh:mm").format(data.getCreationTimestamp())));
-                
-            }
-            
-            
-        });
-        columns.add(new AbstractColumn<EventItem, String>(new Model<String>(""))
-        {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public void populateItem(final Item<ICellPopulator<EventItem>> cellItem,
-                    String componentId, final IModel<EventItem> rowModel) {
-                cellItem.add(new EventActionsPanel(componentId) {
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    protected void onRemoveTriggered(AjaxRequestTarget target) {
-                        removeEvent(rowModel.getObject().getId());
-                        target.add(eventsTable);
-                        target.appendJavaScript("init();");
-                    }
-                    
- 
-                    @Override
-                    protected void onEditTriggered(AjaxRequestTarget target) {
-                        setResponsePage(EditEvent.class, new PageParameters()
-                            .add(EditEvent.EVENT_ID, rowModel.getObject().getId()));
-                        
-                    }
-                });
-                
+            public Panel getPanel(String panelId) {
+                return new EventsListPanel(panelId);
             }
         });
 
-        eventsTable = new DefaultDataTable<>(
-                tableComponentName, columns, new EventsProvider(eventsDAO.getEvents()), ROWS_PER_PAGE);
-        eventsTable.setOutputMarkupId(true);
-        return eventsTable;
+        return new AjaxTabbedPanel<ITab>(tabbedPanelId, tabs);
     }
-    
-    private void removeEvent(Long id) {
-        eventsDAO.removeEvent(id);
-    }
+
 }
